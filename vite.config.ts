a9da88088a3bf9618826df605cbead444a6dac4c
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-24 18:17:42 -0400

    vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "pdp",
-      remotes: {
-        home: "http://localhost:3000/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom", "react-router-dom"],
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "pdp",
+        remotes: {
+          home: "http://localhost:3000/assets/remoteEntry.js",
+        },
+        shared: ["react", "react-dom", "react-router-dom"],
 
-      exposes: {
-        "./PDPContent": "./src/PDPContent.tsx",
-      },
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+        exposes: {
+          "./PDPContent": "./src/PDPContent.tsx",
+        },
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
